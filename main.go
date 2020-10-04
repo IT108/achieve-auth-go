03ff1faeadc7a15ad9093d82d3dc2b593322645e
io112 <gamiysikdima@gmail.com>
2020-10-04 22:36:00 +0300

    models and register route

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import broker "gopkg.in/IT108/achieve-broker-go.v0"
 
-func main()  {
+func main() {
 	base := broker.RouterBase{}
 	authRouter := &authRouter{base}
 	broker.AssignRouter(broker.RouterInterface(authRouter))
+	broker.Subscribe([]string{broker.AUTH_TOPIC}, broker.AUTH_TOPIC)
+	for {}
 }
--- models.go
+++ models.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
+	models "../achieve-models-go"
+	"encoding/json"
 	achieve_broker_go "gopkg.in/IT108/achieve-broker-go.v0"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"log"
 )
 
 type authRouter struct {
 	router achieve_broker_go.RouterBase
 }
 
-func (receiver *authRouter) RunAction(data *kafka.Message)  {
-	fmt.Print("test")
+func (receiver *authRouter) RunAction(data *kafka.Message) {
+	key := string(data.Key)
+	switch key {
+	case models.AUTH_REGISTER_KEY:
+		req := models.RegisterRequest{}
+		json.Unmarshal(data.Value, &req)
+		log.Print(req.Username)
+	default:
+		return
+	}
+
+
 }
